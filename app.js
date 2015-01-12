d8c559c56f8cfee84476606a9d6847c1dd649220
y0u-s <youssef.lakdime@gmail.com>
2015-01-12 20:25:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-var express = require('express');
- 
-var app = express();
- 
-app.get('/', function (req, res) {
-  res.send('hello world');
-});
- 
-app.listen(process.env.PORT || 145.24.222.111:5000);
- 
-module.exports = app;
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, 'APP_PRIVATE_IP_ADDRESS');
+console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
