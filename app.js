ab1e5567443e64bd7d65163566c346caef511b44
y0u-s <youssef.lakdime@gmail.com>
2015-02-19 11:06:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
+var server = app.listen(3000, function () {
 
-  res.send('ewa');
+  var host = server.address().address
+  var port = server.address().port
 
-});
+  console.log('Example app listening at http://%s:%s', host, port)
 
-
-app.listen(8000);
-
-module.exports = app;
+})
